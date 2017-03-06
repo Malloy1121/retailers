6d418ba63c9cf45a3821b2866c9f6483204f7dd1
Malloy <yixiongzhang1121@gmail.com>
2017-03-05 20:28:00 -0500

    add header component

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {AuthService} from "../service/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -6,19 +7,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  private currentCategory:string="ALL";
-  private searchBarGetFocus:boolean=false;
-  constructor() {
+  private currentCategory: string = "All";
+  private searchBarGetFocus: boolean = false;
+  private isAuthenticated: boolean;
+  private categories=["alfkjdalf","sd","a","fdaf","fsadf"];
+
+  @ViewChild("categoryMenu") select: ElementRef;
+
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
+    this.currentCategory = this.select.nativeElement.options[this.select.nativeElement.selectedIndex].text;
+
+    this.isAuthenticated = this.authService.getIsAuthenticated();
   }
 
-  searchBarOnFocus(){
-    this.searchBarGetFocus=true;
+  searchBarOnFocus() {
+    this.searchBarGetFocus = true;
   }
 
-  searchBarOnBlur(){
-    this.searchBarGetFocus=false;
+  searchBarOnBlur() {
+    this.searchBarGetFocus = false;
+  }
+
+  login() {
+    this.isAuthenticated = this.authService.login();
+  }
+
+  logout() {
+    this.isAuthenticated = this.authService.logout();
+  }
+
+  categoryOnClick(category){
+    console.log(category);
+    this.currentCategory=category;
   }
 }
--- client/src/app/service/auth.service.ts
+++ client/src/app/service/auth.service.ts
@@ -2,7 +2,28 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
-
+  private isAuthenticated:boolean=false;
   constructor() { }
+
+  login(){
+    this.isAuthenticated=true;
+    const isAuthenticated=this.isAuthenticated;
+    return isAuthenticated;
+  }
+
+  logout(){
+    this.isAuthenticated=false;
+    const isAuthenticated=this.isAuthenticated;
+    return isAuthenticated;
+  }
+
+  signUp(){
+
+  }
+
+  getIsAuthenticated(){
+    const isAuthenticated=this.isAuthenticated
+    return isAuthenticated;
+  }
 
 }
